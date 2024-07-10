ecd9fa6b305cf961ed5fd7424434747d89a7daeb
RShnitser <romanshnitser@hotmail.com>
2024-07-10 19:17:43 -0400

    complete port to pygame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#from window import Window
 import pygame, sys
 import numpy as np
+import random
 from pygame.locals import *
 
 BOARD_W = 7
@@ -12,14 +12,143 @@ BLUE = (0, 0, 255)
 GRAY = (150, 150, 150)
 DARK_GRAY = (80, 80, 80)
 BLACK = (0, 0, 0, 0)
+AI_DELAY = 0.5
+
+ID_E = 0
+ID_P1 = 1
+ID_P2 = 2
+
+class Player:
+    def __init__(self, id: int, is_ai = False):
+        self.id = id
+        self.is_ai = is_ai
+        self.win_count = 0
 
 class GameState:
     def __init__(self):
          self.board = np.full(BOARD_W * BOARD_H, 0)
+         self.players = []
+         self.players.append(Player(ID_P1, True))
+         self.players.append(Player(ID_P2, True))
+         self.current_player = 0
+         self.moves_left = BOARD_W * BOARD_H
+         self.ai_delay = AI_DELAY
+
+def set_cell(state: GameState, x: int, y: int, v: int):
+        state.board[y * BOARD_W + x] = v
 
 def get_cell(state: GameState, x: int, y: int)-> int:
         result = state.board[y * BOARD_W + x]
         return result
+
+def check_game_over(state: GameState, x: int, y: int, player_id: int)->tuple[bool, int]:
+        count = 0
+        for col in range(BOARD_W):
+            v = get_cell(state, col, y)
+            if v == player_id:
+                count += 1
+            else:
+                count = 0
+            if count >= 4:
+                return True, player_id
+        
+        count = 0
+        for row in range(BOARD_H):
+            v = get_cell(state, x, row)
+            if v == player_id:
+                count += 1
+            else:
+                count = 0
+            if count >= 4:
+                return True, player_id
+        
+        count = 0
+        curr_x = 0
+        curr_y = 0
+        if x > BOARD_H - 1 - y:
+            curr_x = x - (BOARD_H - y - 1)
+            curr_y = BOARD_H - 1
+        else:
+            curr_x = 0
+            curr_y = y + x
+        while curr_x < BOARD_W and curr_y < BOARD_H:
+            v = get_cell(state, curr_x, curr_y)
+            if v == player_id:
+                count += 1
+            else:
+                count = 0
+            curr_x += 1
+            curr_y -= 1
+            if count >= 4:
+                return True, player_id
+        
+        count = 0
+        curr_x = 0
+        curr_y = 0
+        if y > x:
+            curr_x = 0
+            curr_y = y - x
+        else:
+            curr_x = x - y
+            curr_y = 0
+        while curr_x < BOARD_W and curr_y < BOARD_H:
+            v = get_cell(state, curr_x, curr_y)
+            if v == player_id:
+                count += 1
+            else:
+                count = 0
+            curr_x += 1
+            curr_y += 1
+            if count >= 4:
+                return True, player_id
+
+        if state.moves_left == 0:
+            return True, 0
+        return False, 0
+
+def get_empty_cols(state: GameState)->list[int]:
+    result = []
+    for i in range(BOARD_W):
+        v = get_cell(state, i, 0)
+        if v == ID_E:
+            result.append(i)
+    return result
+    
+def reset_board(state: GameState):
+    for i in range(len(state.board)):
+        state.board[i] = 0
+    state.current_player = 0
+    state.moves_left = BOARD_W * BOARD_H
+    state.ai_delay = AI_DELAY
+
+def set_column(state: GameState, x: int, player_id: int):
+    if x < 0 or x >= BOARD_W:
+        return
+    
+    for y in range(BOARD_H - 1, -1, -1):
+        if get_cell(state, x, y) == 0:
+            set_cell(state, x, y, player_id)
+            state.moves_left -= 1
+            gameover_result = check_game_over(state, x, y, player_id)
+            if gameover_result[0]:
+                if gameover_result[1] != ID_E:
+                    state.players[state.current_player].win_count += 1
+                reset_board(state)
+                return
+
+            if state.current_player == 0:
+                state.current_player = 1
+            else:
+                state.current_player = 0
+            return
+        
+def ai_move(state: GameState, player_id: int, dt: float):
+    if state.ai_delay <= 0:
+        cols = get_empty_cols(state)
+        col = cols[random.randrange(len(cols))]
+        set_column(state, col, player_id)
+        state.ai_delay = AI_DELAY
+    state.ai_delay -= dt
 
 def main():
 
@@ -27,12 +156,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((BOARD_W * CELL_SIZE, BOARD_H * CELL_SIZE))
     pygame.display.set_caption("Connect Four AI")
-    #font = pygame.font.SysFont(None, 30)
     font = pygame.font.Font(pygame.font.get_default_font(), 15)
     timer = pygame.time.Clock()
-    #win = Window(100)
-    #win.draw_board()
-    #win.wait_for_close()
     running = True
 
     state = GameState()
@@ -48,31 +173,32 @@ def main():
             if e.type == pygame.MOUSEBUTTONDOWN:
                 buttons = pygame.mouse.get_pressed()
                 if buttons[0]:
-                    m_pos = pygame.mouse.get_pos()
-                    x = m_pos[0] // CELL_SIZE
-                    print(x)
+                    if state.players[state.current_player].is_ai == False:
+                        m_pos = pygame.mouse.get_pos()
+                        m_x = m_pos[0] // CELL_SIZE
+                        set_column(state, m_x, state.players[state.current_player].id)
+
+        if state.players[state.current_player].is_ai == True:
+            ai_move(state, state.players[state.current_player].id, dt)
 
         for y in range(BOARD_H):
             for x in range(BOARD_W):
                 l = x * CELL_SIZE
                 t = y * CELL_SIZE
-                #r = l + CELL_SIZE
-                #b = t + CELL_SIZE
-                #self.__canvas.create_rectangle(l, t, r, b, width=5, fill="gray70")
+            
                 pygame.draw.rect(screen, GRAY, [l, t, CELL_SIZE, CELL_SIZE], 0)
                 pygame.draw.rect(screen, BLACK, [l, t, CELL_SIZE, CELL_SIZE], 2)
 
                 v = get_cell(state, x, y)
                 col = DARK_GRAY
-                if v == 1:
+                if v == ID_P1:
                     col = BLUE
-                elif v == 2:
+                elif v == ID_P2:
                     col = RED
                 m = CELL_SIZE * .2
-                #self.__canvas.create_oval(l + m, t + m, r - m, b - m, fill=col, width=2)
+            
                 pygame.draw.ellipse(screen, col, [l + m, t + m, CELL_SIZE - 2 * m, CELL_SIZE - 2 * m], 0)
                 pygame.draw.ellipse(screen, BLACK, [l + m, t + m, CELL_SIZE - 2 * m, CELL_SIZE - 2 * m], 2)
-                #self.__canvas.create_text(l + m, t + m, text=f"{x},{y}", fill="black", font=("Helvetica 15 bold"))
                 text_render = font.render(f"{x},{y}", True, BLACK)
                 screen.blit(text_render, (l + m, t + m))
 
