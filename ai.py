9ade23dba4db266a715076ec5bc17c3ee7088d2e
RShnitser <romanshnitser@hotmail.com>
2024-07-14 19:02:21 -0400

    add activation functions

--- ai.py
+++ ai.py
@@ -6,7 +6,6 @@ np.random.seed(0)
 class LayerDense:
     weights: npt.NDArray[np.float32]
     biases: npt.NDArray[np.float32]
-    output: npt.NDArray[np.float32]
 
 def create_layer(n_inputs: int, n_neurons) ->LayerDense:
     layer = LayerDense()
@@ -15,5 +14,15 @@ def create_layer(n_inputs: int, n_neurons) ->LayerDense:
 
     return layer
 
-def forward(layer: LayerDense, inputs: npt.NDArray[np.float32]) ->None:
-    layer.output = np.dot(inputs, layer.weights) + layer.biases
+def forward(layer: LayerDense, inputs: npt.NDArray[np.float32]) ->npt.NDArray[np.float32]:
+    result = np.dot(inputs, layer.weights) + layer.biases
+    return result
+
+def relu_forward(input: npt.NDArray[np.float32])->npt.NDArray[np.float32]:
+   result = np.maximum(0, input)
+   return result
+
+def softmax_forward(input: npt.NDArray[np.float32])->npt.NDArray[np.float32]:
+    exp_values = np.exp(input - np.max(input, axis=1, keepdims=True))
+    probabilities = exp_values / np.sum(exp_values, axis=1,keepdims=True)
+    return probabilities
